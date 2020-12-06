83a27545535eb0d1bc17d897b4a18506997ca19c
jsakamoto <junichi.jp@gmail.com>
2020-12-06 13:24:57 +0900

    Fix: CSS live reloader break filtered HTML in some cases.

--- Toolbelt.AspNetCore.CssLiveReloader/Internals/InjectReloaderScriptMiddleware.cs
+++ Toolbelt.AspNetCore.CssLiveReloader/Internals/InjectReloaderScriptMiddleware.cs
@@ -39,7 +39,7 @@ namespace Toolbelt.AspNetCore.CssLiveReloader.Internals
                     filter.MemoryStream.SetLength(0);
                     var encoding = Encoding.UTF8;
                     using var writer = new StreamWriter(filter.MemoryStream, bufferSize: -1, leaveOpen: true, encoding: encoding) { AutoFlush = true };
-                    doc.ToHtml(writer, new PrettyMarkupFormatter());
+                    doc.ToHtml(writer, new HtmlMarkupFormatter());
 
                     context.Response.ContentLength = filter.MemoryStream.Length;
                     filter.MemoryStream.Seek(0, SeekOrigin.Begin);
